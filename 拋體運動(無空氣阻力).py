94473bc656b80d363991accff2a67eb7ee4a637d
jason-jasom <78861576+jason-jasom@users.noreply.github.com>
2021-02-14 18:47:45 +0800

    Update 拋體運動(無空氣阻力).py

--- 拋體運動(無空氣阻力).py
+++ 拋體運動(無空氣阻力).py
@@ -1,10 +1,10 @@
 import math as m
 while True:
-    h=float(input('height:'))#起始高度
-    v=float(input('velocity:'))#速度
-    d=float(input('degree:'))#角度
-    t=float(input('time:'))#時間
-    g=float(input('gravity:'))#重力加速度
+    h=float(input('起始高度(公尺):'))
+    v=float(input('速度(公尺/秒):'))
+    d=float(input('角度(度):'))
+    t=float(input('時間(秒):'))
+    g=float(input('重力加速度(公尺/秒平方):'))
     s=m.sin(m.pi*d/180)#sin
     c=m.cos(m.pi*d/180)#cos
     T=v*s/g+s*m.sqrt(v*v+2*g*h)/g#飛行時間
